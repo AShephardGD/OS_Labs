8d4fb16956341f77addb8571cb6bd9a0069f9673
Adrian Shephard <Koleg02@mail.ru>
2022-12-05 03:47:53 +0300

    Have made Shared Memory Lab

---
+++ shmem3/first.c
@@ -0,0 +1,55 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<errno.h>
+#include<signal.h>
+#include<string.h>
+
+const size_t size = 256;
+const int id = 228;
+const char* path = "./shmem";
+
+int shmid = 0;
+
+char* getCurrentTime() {
+	time_t rawtime;
+	struct tm* info;
+	time(&rawtime);
+	info = localtime(&rawtime);
+	return asctime(info);
+}
+
+void sigHandler() {
+	if (shmid) {	
+		shmctl(shmid, IPC_RMID, NULL);
+	}
+	exit(1);
+}
+
+void first() {
+	signal(SIGINT, sigHandler);
+	signal(SIGTERM, sigHandler);
+	key_t key = ftok(path, id);
+	shmid = shmget(key, size, 0660 | IPC_CREAT | IPC_EXCL);
+	if ((shmid == -1) && (errno == EEXIST)) {
+		printf("Программа уже запущена!\n");
+		exit(1);
+	}
+	while (1) {
+		char str[size];
+		sprintf(str, "%d: %s", getpid(), getCurrentTime());
+		printf("%s", str);
+		char* mem = (char*) shmat(shmid, NULL, 0);
+		memset(mem, 0, size);
+		strcpy(mem, str);
+		sleep(5);
+	}	
+}
+
+int main() {
+	first(); // Создать две независимых программыю В бесконечном цикле передавать из первого процесса (первая программа) через разделяемую память текущее время и pid передающего процесса. Принимающий процесс (вторая программа) должен выводить на экран свое текущее время и pid и принятую строчку. Обязательно: Вторую программу можно запускать параллельно несколько раз, первая программа (передающая) должна запускаться только один раз. Повторный запуск первой программы, если уже есть процесс с такой программой, должен завершаться с соответствующим сообщением на экране.
+	return 0;
+}
---
+++ shmem3/second.c
@@ -0,0 +1,36 @@
+#include<time.h>
+#include<sys/types.h>
+#include<sys/shm.h>
+#include<sys/ipc.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+const size_t size = 256;
+const int id = 228;
+const char* path = "./shmem";
+
+char* getCurrentTime() {
+	time_t rawtime;
+	struct tm* info;
+	time(&rawtime);
+	info = localtime(&rawtime);
+	return asctime(info);
+}
+
+void second() {
+	key_t key = ftok(path, id);
+	int shmid = shmget(key, size, 0);
+	if (shmid == -1) {
+		printf("Запустите передающий процесс!\n");
+		exit(1);
+	}
+	char* mem = (char*) shmat(shmid, NULL, 0);
+	printf("%d: %s%s\n", getpid(), getCurrentTime(), mem);
+	shmdt(mem);
+}
+
+int main() {
+	second(); // Создать две независимых программы. В бесконечном цикле передавать из первого процесса (первая программа) через разделяемую память текущее время и pid передающего процесс. Принимающий процесс (вторая программа) должен выводить на экран свое текущее время и pid и принятую строчку. Обязательно: Вторую программу можно запускать параллельно несколько раз, первая программа (передающая) должна запускаться только один раз. Повторный запуск первой программы, если уже есть процесс с такой программой, должен завершаться с соответствующим сообщением на экране.
+	return 0;
+}
