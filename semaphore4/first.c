cc63eae1044ecbe723bccad06724e53fa5777279
Adrian Shephard <Koleg02@mail.ru>
2022-12-11 02:01:07 +0300

    semaphore lab is ready

--- semaphore4/first.c
+++ semaphore4/first.c
@@ -4,13 +4,17 @@
 #include<stdlib.h>
 #include<sys/ipc.h>
 #include<sys/shm.h>
+#include<sys/stat.h>
 #include<errno.h>
 #include<signal.h>
 #include<string.h>
+#include<fcntl.h>
+#include<semaphore.h>
 
 const size_t size = 256;
 const int id = 228;
 const char* path = "./shmem";
+const char* semName = "/yomom";
 
 int shmid = 0;
 
@@ -23,7 +27,7 @@ char* getCurrentTime() {
 }
 
 void sigHandler() {
-	if (shmid) {	
+	if (shmid) {
 		shmctl(shmid, IPC_RMID, NULL);
 	}
 	exit(1);
@@ -33,7 +37,7 @@ void first() {
 	signal(SIGINT, sigHandler);
 	signal(SIGTERM, sigHandler);
 	key_t key = ftok(path, id);
-	shmid = shmget(key, size, 0660 | IPC_CREAT | IPC_EXCL);
+	shmid = shmget(key, size, 0666 | IPC_CREAT | IPC_EXCL);
 	if ((shmid == -1) && (errno == EEXIST)) {
 		printf("Программа уже запущена!\n");
 		exit(1);
@@ -42,7 +46,15 @@ void first() {
 		printf("Не удалось получить разделяемую память");
 		exit(1);
 	}
+	sem_unlink(semName);
+	sem_t* sem = sem_open(semName, O_CREAT, 0666, 1);
+	if (sem == SEM_FAILED) {
+		perror("SEM_FAILED");
+		exit(1);
+	}
 	while (1) {
+		printf("First: sem_wait()\n");
+		sem_wait(sem);
 		char str[size];
 		sprintf(str, "%d: %s", getpid(), getCurrentTime());
 		printf("%s", str);
@@ -50,15 +62,19 @@ void first() {
 		memset(mem, 0, size);
 		strcpy(mem, str);
 		sleep(5);
-	}	
+		printf("First: sem_post()\n\n");
+		sem_post(sem);
+		sleep(5);
+	}
 }
 
 int main() {
-	first(); // Создать две независимых программы.
+	first(); // Создать две независимые программы.
 	         // В бесконечном цикле передавать из первого процесса (первая программа) через разделяемую память текущее время и pid передающего процесса.
 	         // Принимающий процесс (вторая программа) должен выводить на экран свое текущее время и pid и принятую строчку.
 	         // Обязательно:
 	         // Вторую программу можно запускать параллельно несколько раз, первая программа (передающая) должна запускаться только один раз.
 	         // Повторный запуск первой программы, если уже есть процесс с такой программой, должен завершаться с соответствующим сообщением на экране.
+	         // Для синхронизации обмена данными использовать semaphore.
 	return 0;
 }
---
+++ semaphore4/second.c
@@ -0,0 +1,52 @@
+#include<time.h>
+#include<sys/types.h>
+#include<sys/shm.h>
+#include<sys/ipc.h>
+#include<sys/stat.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<semaphore.h>
+
+const size_t size = 256;
+const int id = 228;
+const char* path = "./shmem";
+const char* semName = "/yomom";
+
+char* getCurrentTime() {
+	time_t rawtime;
+	struct tm* info;
+	time(&rawtime);
+	info = localtime(&rawtime);
+	return asctime(info);
+}
+
+void second() {
+	key_t key = ftok(path, id);
+	int shmid = shmget(key, size, 0);
+	if (shmid == -1) {
+		printf("Запустите передающий процесс!\n");
+		exit(1);
+	}
+	sem_t* sem = sem_open(semName, 0);
+	printf("Second: sem_wait()\n");
+	sem_wait(sem);
+	char* mem = (char*) shmat(shmid, NULL, 0);
+	printf("%d: %s%s", getpid(), getCurrentTime(), mem);
+	shmdt(mem);
+	sleep(5);
+	printf("Second: sem_post()\n");
+	sem_post(sem);
+}
+
+int main() {
+	second(); // Создать две независимых программы.
+	          // В бесконечном цикле передавать из первого процесса (первая программа) через разделяемую память текущее время и pid передающего процесса.
+	          // Принимающий процесс (вторая программа) должен выводить на экран свое текущее время и pid и принятую строчку.
+	          // Обязательно:
+	          // Вторую программу можно запускать параллельно несколько раз, первая программа (передающая) должна запускать только один раз.
+	          // Повторный запуск первой программы, если уже есть процесс с такой программой, должен завершаться с соответствующим сообщением на экране.
+	          // Для синхронизации обмена данными использовать semaphore.
+	return 0;
+}
